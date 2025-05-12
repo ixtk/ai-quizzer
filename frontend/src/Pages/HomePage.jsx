f7814876263388c6a9e0cc3f14abc5b0dbcd55c0
sallomeN <nozadzesalome763@gmail.com>
2025-05-12 23:20:58 +0400

    make_css_less_painful

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,4 +1,36 @@
+import "../App.css"
 function HomePage() {
-  return <h1>Home</h1>
+  return (
+    <div className="container">
+      <h1>Home</h1>
+
+      <div className="card">
+        <button className="btn btn-primary">Click Me</button>
+        <button className="btn btn-outline">Click Me</button>
+        <button className="btn btn-secondary">Click Me</button>
+      </div>
+
+      <br />
+
+      <div className="card">
+        <div>
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" />
+        </div>
+
+        <br />
+
+        <div>
+          <label htmlFor="story">Story</label>
+          <textarea rows={5} id="story"></textarea>
+        </div>
+      </div>
+
+      <br />
+
+      <span className="badge badge-danger">TRUE</span>
+      <span className="badge badge-success">FALSE</span>
+    </div>
+  )
 }
 export default HomePage
